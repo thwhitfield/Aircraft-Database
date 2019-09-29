9b2ea88754c3cdcacf7c72596a8e4daccad162f1
thwhitfield <thwhitfield@gmail.com>
2019-09-29 13:49:29 -0400

    updates

--- best_matches.js
+++ best_matches.js
@@ -67,22 +67,22 @@ function jsonLoad(callback) {
     xhr.send();
 }
 
-// function CustomTooltip () {}
+function CustomTooltip () {}
 
-// CustomTooltip.prototype.init = function(params) {
-    // var eGui = this.eGui = document.createElement('div');
-    // var color = params.color || 'white';
-    // var data = params.api.getDisplayedRowAtIndex(params.rowIndex).data;
+CustomTooltip.prototype.init = function(params) {
+    var eGui = this.eGui = document.createElement('div');
+    var color = params.color || 'white';
+    var data = params.api.getDisplayedRowAtIndex(params.rowIndex).data;
 
 
-    // eGui.classList.add('custom-tooltip');
-    // eGui.style['background-color'] = color;
-    // eGui.innerHTML =
-        // '<p><span class"name">' + data.aircraft + '</span></p>';
-        '<p><span>Country: </span>' + data.country + '</p>' +
-        '<p><span>Total: </span>' + data.total + '</p>';
-// };
+    eGui.classList.add('custom-tooltip');
+    eGui.style['background-color'] = color;
+    eGui.innerHTML =
+        '<p><span class"name">' + data.aircraft + '</span></p>';
+        // '<p><span>Country: </span>' + data.country + '</p>' +
+        // '<p><span>Total: </span>' + data.total + '</p>';
+};
 
-// CustomTooltip.prototype.getGui = function() {
-    // return this.eGui;
-// };
+CustomTooltip.prototype.getGui = function() {
+    return this.eGui;
+};
